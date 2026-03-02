1e8788811dc984d4a1eb4327c8b22ce723591c4a
WesMarquess <weslleymarquess101@gmail.com>
2026-03-01 22:59:28 -0300

    Adição do método toString utilizando o StringBuilder

--- src/Arrays/Main.java
+++ src/Arrays/Main.java
@@ -6,5 +6,9 @@ public class Main {
         Vetor vetor = new Vetor(5);
         vetor.adicionaElementos("Wes");
         vetor.adicionaElementos("Wes 2");
+
+        System.out.println(vetor.tamanho());
+
+        System.out.println(vetor);
     }
 }
--- src/Arrays/Vetor.java
+++ src/Arrays/Vetor.java
@@ -12,9 +12,32 @@ public class Vetor {
     public boolean adicionaElementos(String elemento) {
         if (this.tamanho < this.elementos.length) {
             this.elementos[this.tamanho] = elemento;
+            this.tamanho++;
             return true;
         }
-
         return false;
+    }
+
+    public int tamanho() {
+        return this.tamanho;
+    }
+
+    @Override
+    public String toString() {
+
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append("[");
+
+        for (int i = 0; i < this.tamanho - 1; i++) {
+            stringBuilder.append(this.elementos[i]);
+            stringBuilder.append(" , ");
+        }
+
+        if (this.tamanho > 0) {
+            stringBuilder.append(this.elementos[this.tamanho - 1]);
+        }
+        stringBuilder.append(" ]");
+
+        return stringBuilder.toString();
     }
 }
