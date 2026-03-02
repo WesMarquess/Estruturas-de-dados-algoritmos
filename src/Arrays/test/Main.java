18f24dc557950e5cf54a6b6deeb31504e1818423
WesMarquess <weslleymarquess101@gmail.com>
2026-03-01 22:39:34 -0300

    Initial commit

---
+++ src/Arrays/test/Main.java
@@ -0,0 +1,12 @@
+package Arrays.test;
+import Arrays.Vetor;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Vetor vetor = new Vetor(2);
+        vetor.adicionarElemento("elemento 1");
+        vetor.adicionarElemento("elemento 2");
+
+    }
+}
